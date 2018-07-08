7262bf0dad330a6143cc3c80f9ad14ed8a606a84
alexpchin <alex@alexchin.co.uk>
2018-07-08 22:38:38 +0100

    Lesson 28

--- albums/index.js
+++ albums/index.js
@@ -1,10 +1,11 @@
 // Import a library to create a component
 import React from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a component
 const App = () => (
-  <Text>Some Text</Text> 
+  <Header />
 );
 
 // Render it to the device
