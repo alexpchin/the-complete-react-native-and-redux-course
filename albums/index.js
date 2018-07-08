d20289c48b7260bbe9908e68592ae9fc63b445ca
alexpchin <alex@alexchin.co.uk>
2018-07-08 22:54:51 +0100

    Lesson 31 Class-based components

--- albums/index.js
+++ albums/index.js
@@ -1,11 +1,15 @@
 // Import a library to create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-  <Header />
+  <View>
+    <Header headerText={'Albums!'} />
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
