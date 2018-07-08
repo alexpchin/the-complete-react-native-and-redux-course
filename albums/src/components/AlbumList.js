4e8fdd41ff1a655096ec9489370db65fb724b478
alexpchin <alex@alexchin.co.uk>
2018-07-09 00:43:31 +0100

    Lesson 38

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = {
+    albums: []
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{ album.title }</Text>);
+  }
+  
   render() {
+    console.log(this.state);
+
     return (
       <View>
-        <Text>AlbumList</Text>
+        { this.renderAlbums() }
       </View>
     );
   }
