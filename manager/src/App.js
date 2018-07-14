6c0a241d138cddb7d8c1779c838f7ae74f200019
alexpchin <alex@alexchin.co.uk>
2018-07-14 21:16:46 +0100

    Finishing 108

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'; 
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCtaD9mg9Gj9oj2L6s3LlxCsaQIwBrGezg',
+      authDomain: 'manager-ad9b8.firebaseapp.com',
+      databaseURL: 'https://manager-ad9b8.firebaseio.com',
+      projectId: 'manager-ad9b8',
+      storageBucket: 'manager-ad9b8.appspot.com',
+      messagingSenderId: '848947302266'
+    };
+
+    firebase.initializeApp(config);
+  }
+
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello
