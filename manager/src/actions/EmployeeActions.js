b9118bf479879db65d977ff1a85456254a2cef23
alexpchin <alex@alexchin.co.uk>
2018-07-22 01:48:32 +0100

    Completing 145

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import { 
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 // One action creator that can update any prop
@@ -20,10 +21,21 @@ export const employeeCreate = ({ name, phone, shift }) => {
   // Use redux thunk
   return (dispatch) => {
     firebase.database().ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift })
-    .then(() => {
-      dispatch({ type: EMPLOYEE_CREATE });
-      Actions.pop();
-    });
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.pop();
+      });
+  };
+};
+
+export const employeeFetch = () => {
+  const { currentUser } = firebase.auth();
+  
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+      });
   };
 };
