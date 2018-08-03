a2d57853d283b20531c1d732b85f182eaa9c5e83
alexpchin <alex@alexchin.co.uk>
2018-08-03 14:29:54 +0100

    Adding enviroment variables with babel-plugin-transform-inline-environment-variables

--- manager/src/App.js
+++ manager/src/App.js
@@ -11,13 +11,15 @@ class App extends Component {
 
   componentWillMount() {
     const config = {
-      apiKey: 'AIzaSyCtaD9mg9Gj9oj2L6s3LlxCsaQIwBrGezg',
-      authDomain: 'manager-ad9b8.firebaseapp.com',
-      databaseURL: 'https://manager-ad9b8.firebaseio.com',
-      projectId: 'manager-ad9b8',
-      storageBucket: 'manager-ad9b8.appspot.com',
-      messagingSenderId: '848947302266'
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
     };
+
+    console.log(config);
 
     firebase.initializeApp(config);
   }
