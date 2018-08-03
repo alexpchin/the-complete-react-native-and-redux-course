784e4243293ec9cf4eb234fd4ce7fba765f87a25
alexpchin <alex@alexchin.co.uk>
2018-08-03 13:35:09 +0100

    Completing 162

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
-import Communications from 'react-native-communications';
+import { text } from 'react-native-communications';
 import React, { Component } from 'react';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false }
+
   componentWillMount() {
     // We are going to iterate over the object
     _.each(this.props.employee, (value, prop) => {
@@ -21,7 +23,15 @@ class EmployeeEdit extends Component {
 
   onTextPress() {
     const { phone, shift } = this.props;
-    Communications.text(phone, `Your upcoming shift is on ${shift}`);
+    text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
+  onAccept() {
+    this.props.employeeDelete({ uid: this.props.employee.uid });
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
   }
 
   render() {
@@ -38,6 +48,19 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm 
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
@@ -49,5 +72,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, { 
-  employeeUpdate, employeeSave 
+  employeeUpdate, employeeSave, employeeDelete 
 })(EmployeeEdit);
--- manager/src/components/common/Confirm.js
+++ manager/src/components/common/Confirm.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Text, View, Modal } from 'react-native';
-import { CardSection } from './common';
-import { Button } from './common';
+import { CardSection } from '../common';
+import { Button } from '../common';
 
 const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { containerStyle, textStyle, cardSectionStyle } = styles;
+
   return (
     <Modal
       visible={visible}
@@ -11,18 +13,36 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
       animationType="slide"
       onRequestClose={() => {}}
     >
-      <View>
-        <CardSection>
-          <Text>{children}</Text>
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>{children}</Text>
         </CardSection>
 
         <CardSection>
-          <Button onPress="onAccept">Yes</Button>
-          <Button onPress="onDecline">No</Button>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
         </CardSection>
       </View>
     </Modal>
   );
+};
+
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, .75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
 };
 
 export { Confirm };
